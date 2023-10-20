e6248d963ac7ab5e4482a461164117479fda98d9
Fehmi ANAÇ <fehmi_anac@hotmail.com>
2023-10-20 16:17:57 +0300

    Update PagedResponse.cs

--- src/Domain/Dto/PagedResponse.cs
+++ src/Domain/Dto/PagedResponse.cs
@@ -1,9 +1,17 @@
 namespace Domain.Dto;
 
 public class PagedResponse<T>
-{
-    public IList<T> Data { get; set; } = default!;
-    public string? NextToken { get; set; }
-    public int Limit { get; set; }
-    public string? PreviousToken { get; set; }
-}
+    {
+        public IList<T> Data { get; set; } = default!;
+
+        private string? _nextToken;
+
+        public string? NextToken
+        {
+            get => _nextToken;
+            set => _nextToken = value != "%7b%7d" ? value : null;
+        }
+
+        public int Limit { get; set; }
+        public string? PreviousToken { get; set; }
+    }
